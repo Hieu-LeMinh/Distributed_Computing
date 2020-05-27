c7b259bc9728b1c04f2aa1e026ab75fdd5046fc2
Lelinh <leqlinh1998@gmail.com>
2020-05-28 02:33:48 +0700

    Update CNM

--- CNM.py
+++ CNM.py
@@ -1,15 +1,32 @@
 import snap
+import matplotlib.pyplot as plt
 
-def CNMGraph(Graph):
-	CmtyV = snap.TCnComV()
-	modularity = snap.CommunityCNM(Graph, CmtyV)
-	for Cmty in CmtyV:
-	    print("Community: ")
-	    for NI in Cmty:
-	        print(NI)
-	print("The modularity of the network is %f" % modularity)
+G1 = snap.LoadEdgeList(snap.PUNGraph, "musae_facebook_edges.csv", 0, 1, ',')
 
-if __name__ == '__main__':
-	G1 = snap.LoadEdgeList(snap.PUNGraph, "musae_facebook_edges.csv", 0, 1, ',')
+CmtyV = snap.TCnComV()
+modularity = snap.CommunityCNM(G1, CmtyV)
+count = 0
+sizes = []
+communities = []
+for Cmty in CmtyV:
+    listcmty = []
+    for NI in Cmty:
+        listcmty.append(NI)
 
-	CNMGraph(G1)
+    communities.append(listcmty)
+    count += 1
+    sizes.append(len(listcmty))
+print("Number of communities:", count)
+print("Largest community:", max(sizes))
+print("Smallest community:", min(sizes))
+
+# plot histogram of community sizes
+sizes.sort()
+plt.hist(sizes, log=True)
+plt.xlabel("Size of community")
+plt.ylabel("Number of communities")
+plt.title("Sizes of CNM communities")
+plt.show()
+plt.savefig("cnm-sizes.png")
+#print("Size of communities:", sizes)
+print("The modularity of the network is %f" % modularity)
